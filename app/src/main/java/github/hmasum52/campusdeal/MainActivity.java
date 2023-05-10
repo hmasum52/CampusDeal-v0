bbbd2f66e1793045fe5c86873f74e9e2599f82ef
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2023-05-10 17:34:05 +0600

    added bottom navigation bar with basic app fragments

--- app/src/main/java/github/hmasum52/campusdeal/MainActivity.java
+++ app/src/main/java/github/hmasum52/campusdeal/MainActivity.java
@@ -1,6 +1,9 @@
 package github.hmasum52.campusdeal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -15,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mVB = ActivityMainBinding.inflate(super.getLayoutInflater());
         super.setContentView(mVB.getRoot());
+
+        // set up bottom navigation bar
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_frag);
+        // after set up with navController, it will automatically handle the navigation
+        // when user click on the bottom navigation bar
+        NavigationUI.setupWithNavController(mVB.bottomNavigation, navController);
     }
 }
