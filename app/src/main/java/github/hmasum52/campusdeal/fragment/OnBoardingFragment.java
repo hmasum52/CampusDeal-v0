53fd2bf410640ba9505922d6bdd55f3bcd26a279
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2023-08-13 23:18:13 +0600

    firebase authentication done

--- app/src/main/java/github/hmasum52/campusdeal/fragment/OnBoardingFragment.java
+++ app/src/main/java/github/hmasum52/campusdeal/fragment/OnBoardingFragment.java
@@ -1,23 +1,104 @@
 package github.hmasum52.campusdeal.fragment;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import dagger.hilt.android.AndroidEntryPoint;
-import github.hmasum52.campusdeal.R;
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-//@AndroidEntryPoint
+import java.util.Arrays;
+import java.util.List;
+
+import github.hmasum52.campusdeal.R;
+import github.hmasum52.campusdeal.databinding.FragmentLoginBinding;
+import github.hmasum52.campusdeal.databinding.FragmentOnBoardingBinding;
+
+
 public class OnBoardingFragment extends Fragment {
+
+    private FragmentOnBoardingBinding mVB;
+
+
+    // firebase auth
+    // See: https://developer.android.com/training/basics/intents/result
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
+    );
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_boarding, container, false);
+        mVB = FragmentOnBoardingBinding.inflate(inflater, container, false);
+        return mVB.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+        AuthMethodPickerLayout customLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.fragment_login)
+                .setGoogleButtonId(R.id.google_sign_in_btn)
+                .setEmailButtonId(R.id.email_sing_in_btn)
+                .build();
+
+        // Choose authentication providers
+        // add email and google sign in and forget password
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        // https://github.com/firebase/FirebaseUI-Android/issues/229#issuecomment-236868365
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTheme(R.style.Theme_CampusDeal)
+                .setAuthMethodPickerLayout(customLayout)
+                .build();
+
+
+        mVB.getStartBtn.setOnClickListener(v -> {
+            signInLauncher.launch(signInIntent);
+        });
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Log.d("MainActivity", user.toString());
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
     }
 }
