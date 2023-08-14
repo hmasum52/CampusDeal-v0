1a893f8746ca301487b1b0e918f192c18bad4ebd
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2023-08-14 17:06:17 +0600

    added HiltAndroidApp

---
+++ app/src/main/java/github/hmasum52/campusdeal/CampusDealApplication.java
@@ -0,0 +1,9 @@
+package github.hmasum52.campusdeal;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class CampusDealApplication extends Application {
+}
